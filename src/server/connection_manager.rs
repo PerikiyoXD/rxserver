ce242378fe778d992826d5b801b74b969b12e978
PerikiyoXD <perikiyoxd@gmail.com>
2025-06-11 00:22:53 +0200

    refactor: Clean up imports and improve code formatting in ConnectionManager

--- src/server/connection_manager.rs
+++ src/server/connection_manager.rs
@@ -1,18 +1,21 @@
 /*!
  * Connection Manager for X11 Server
- * 
+ *
  * Handles network connections (TCP and Unix domain sockets) and manages
  * the lifecycle of client connections.
  */
 
-use crate::{config::ServerConfig, Result, todo_critical, todo_high, todo_medium};
+use crate::{config::ServerConfig, todo_critical, todo_high, Result};
 use std::sync::Arc;
-use tokio::net::{TcpListener, UnixListener};
-use tracing::{info, debug, error};
+use tokio::net::TcpListener;
+#[cfg(unix)]
+use tokio::net::UnixListener;
+use tracing::info;
 
 /// Manages network connections to the X server
 pub struct ConnectionManager {
     tcp_listener: Option<TcpListener>,
+    #[cfg(unix)]
     unix_listener: Option<UnixListener>,
     display_num: u8,
 }
@@ -20,38 +23,50 @@ pub struct ConnectionManager {
 impl ConnectionManager {
     /// Create a new connection manager
     pub fn new(config: &ServerConfig) -> Result<Self> {
-        todo_critical!("connection_manager", "ConnectionManager::new not implemented");
-        
-        info!("Initializing ConnectionManager for display :{}", config.display_num);
-        
+        todo_critical!(
+            "connection_manager",
+            "ConnectionManager::new not implemented"
+        );
+
+        info!(
+            "Initializing ConnectionManager for display :{}",
+            config.display_num
+        );
         Ok(ConnectionManager {
             tcp_listener: None,
+            #[cfg(unix)]
             unix_listener: None,
-            display_num: config.display_num,
+            display_num: 0, // TODO: Extract from config
         })
     }
-    
+
     /// Start listening for connections
     pub async fn start_listening(&mut self) -> Result<()> {
         todo_critical!("connection_manager", "start_listening not implemented");
-        
+
         // TODO: Implement TCP listener setup
         todo_high!("connection_manager", "TCP listener setup not implemented");
-        
-        // TODO: Implement Unix domain socket listener setup  
-        todo_high!("connection_manager", "Unix domain socket listener setup not implemented");
-        
+
+        // TODO: Implement Unix domain socket listener setup
+        todo_high!(
+            "connection_manager",
+            "Unix domain socket listener setup not implemented"
+        );
+
         // TODO: Set up connection acceptance loop
-        todo_high!("connection_manager", "Connection acceptance loop not implemented");
-        
+        todo_high!(
+            "connection_manager",
+            "Connection acceptance loop not implemented"
+        );
+
         Ok(())
     }
-    
+
     /// Accept incoming connections
     pub async fn accept_connections(&self) -> Result<()> {
         todo_critical!("connection_manager", "accept_connections not implemented");
     }
-    
+
     /// Get the display number
     pub fn display_num(&self) -> u8 {
         self.display_num
