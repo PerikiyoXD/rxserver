1d76d16b1884cf27399f2ad04eaa46536f01c4dc
PerikiyoXD <perikiyoxd@gmail.com>
2025-06-11 15:58:43 +0200

    feat: Remove logging module from utility module

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,9 +2,7 @@
 //!
 //! This module contains utility functions and helper modules.
 
-pub mod logging;
 pub mod todo;
 pub mod implementation_status;
 
-pub use logging::*;
 pub use implementation_status::*;
